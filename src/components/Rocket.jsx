d5c259511342eee533dfae198162edc2697239f5
Luiscarlosvd <lc26cr7@gmail.com>
2023-06-19 15:11:42 -0400

    Add Rocket component and with img, name and description for each rocket
    Add buttons for reserve and cancel reservation

---
+++ src/components/Rocket.jsx
@@ -0,0 +1,30 @@
+const Rocket = ({ img, name, description }) => {
+  return (
+    <div className="flex w-11/12 m-auto gap-3 mb-8">
+      <img className="w-60 h-48" src={img} alt="USER" />
+      <div className="flex flex-col gap-3">
+        <h2 className="font-semibold text-xl">{name}</h2>
+        <p>
+          <span className="bg-cyan-500 rounded-md text-white text-xs font-semibold px-1 mr-2">
+            Reserved 
+          </span>
+          {description}
+        </p>
+        <button 
+          type="button"
+          className="w-36 rounded-md text-lg p-2 text-white bg-blue-500"
+        >
+          Reserve Rocket
+        </button>
+        <button 
+          type="button"
+          className="w-44 rounded-md text-lg p-2 text-gray-500 border border-gray-500"
+        >
+          Cancel Reservation
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Rocket
