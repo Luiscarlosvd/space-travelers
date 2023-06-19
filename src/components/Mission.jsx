5c6c6bd22d0d7470070e308a447ba37b56dc3600
Luiscarlosvd <lc26cr7@gmail.com>
2023-06-19 17:42:16 -0400

    Fix problem of size mission buttons

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -4,10 +4,10 @@ import { joinMission, leaveMission } from "../redux/missions/missionsSlice";
 
 const MemberSpan = ({ activeMember }) => {
   return activeMember ? (
-    <span className="text-xs text-white font-bold whitespace-nowrap bg-cyan-600 px-2 py-1 rounded-md">Active Member</span>
+    <div className="text-xs text-white font-bold whitespace-nowrap bg-cyan-600 py-1 w-24 text-center rounded-md">Active Member</div>
   ) : (
-    <span className="text-xs text-white font-bold whitespace-nowrap bg-gray-500 px-2 py-1 rounded-md">Not a member</span>
-  );
+    <div className="text-xs text-white font-bold whitespace-nowrap bg-gray-500 py-1 w-24 text-center rounded-md">Not a member</div>
+  )
 };
 
 function Mission({ name, desc, activeMember, id }) {
@@ -22,12 +22,11 @@ function Mission({ name, desc, activeMember, id }) {
         <MemberSpan activeMember={activeMember} />
       </td>
       <td className="px-4 py-2 border border-gray-300 whitespace-nowrap">
-        {/* <ActionButton activeMember={activeMember} /> */}
         {
           activeMember ? (
-            <button className="px-3 py-1 rounded-md border text-red-600 border-red-600" onClick={() => {dispatch(leaveMission(id))}}>Leave Mission</button>
+            <button className="px-3 py-1 w-32 rounded-md border text-red-600 border-red-600" onClick={() => {dispatch(leaveMission(id))}}>Leave Mission</button>
           ) : (
-            <button className="px-3 py-1 rounded-md border text-gray-600 border-gray-600" onClick={() => {dispatch(joinMission(id))}}>Join Mission</button>
+            <button className="px-3 py-1 w-32 rounded-md border text-gray-600 border-gray-600" onClick={() => {dispatch(joinMission(id))}}>Join Mission</button>
           )
         }
       </td>
