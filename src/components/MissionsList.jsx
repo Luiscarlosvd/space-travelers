113d569c99442ac1ac915778d076175c26898948
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-06-19 14:24:20 -0300

    Add useEffect hook to dispatch getMissions when the component missionsList is rendered

--- src/components/MissionsList.jsx
+++ src/components/MissionsList.jsx
@@ -1,6 +1,20 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getMissions } from '../redux/missions/missionsSlice';
+
 function MissionsList() {
+  const missions = useSelector((store) => store.missions);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMissions());
+  }, [dispatch]);
+
   return (
-    <div>MissionsList</div>
+    <section>
+      missions
+    </section>
   )
 }
 
