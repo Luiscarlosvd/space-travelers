67228c0993320f9ccf48763152fb167f143c203b
Luiscarlosvd <lc26cr7@gmail.com>
2023-06-19 18:19:11 -0400

    Add filteredRockets array for List in My Profile Section

--- src/components/ProfileList.jsx
+++ src/components/ProfileList.jsx
@@ -4,11 +4,14 @@ import { useSelector } from "react-redux";
 
 function ProfileList() {
   const missions = useSelector((store) => store.missions);
-  const filteredMissions = missions.missionsArr.filter((mission)=>mission.activeMember === true)
+  const rockets = useSelector((store) => store.rockets);
+  const filteredRockets = rockets.rocketsArr.filter((rocket) => rocket.reserved === true);
+  const filteredMissions = missions.missionsArr.filter((mission)=>mission.activeMember === true);
+
   return (
     <section className="flex mx-16 w-11/12 gap-16">
       <FilteredList array={filteredMissions} title="My Missions"/>
-      <FilteredList array={filteredMissions} title="My Missions"/>
+      <FilteredList array={filteredRockets} title="My Rockets"/>
     </section>
   )
 }
